56ac2c0e0cc23a3e3bfbcdd6b7c82086cda2b5ec
Sunil Kumar R <71269904+sunilkumar-git14@users.noreply.github.com>
2020-09-22 13:30:29 +0530

    fifth version

--- helloworld.py
+++ helloworld.py
@@ -2,5 +2,8 @@ variable=input("if you like the world:\n")
 if variable=="yes":
     print("welcome to new world")
     print("congrats for coming")
+    print("congrats for coming1")
+    print("congrats for coming2")
+    print("congrats for coming3")
 else:
     print("then do your work")
