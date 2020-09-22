19e7a249bacf6566aad87a962848aac0b4b10da3
Sunil Kumar R <71269904+sunilkumar-git14@users.noreply.github.com>
2020-09-22 15:38:01 +0530

    akbar

--- helloworld.py
+++ helloworld.py
@@ -2,8 +2,8 @@ variable=input("if you like the world:\n")
 if variable=="yes":
     print("welcome to new world")
     print("congrats for coming")
-    print("sunil")
     print("akbar")
-    print("malli")
+    print("akbar")
+    print("akbar")
 else:
     print("then do your work")
