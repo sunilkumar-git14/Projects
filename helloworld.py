368f3a610645cf4f166c191ba6175e5568a6beef
Sunil Kumar R <71269904+sunilkumar-git14@users.noreply.github.com>
2020-09-30 09:47:03 +0530

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-variable=input("if you like the world:\n")
-if variable=="yes":
-    print("welcome to new world")
-    print("congrats for coming")
-    print("sunil")
-    print("sunil")
-    print("sunil")
-else:
-    print("then do your work")
